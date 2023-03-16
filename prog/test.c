9099b4798417220b439d28e1ffe3465049652c5e
walternoguera <walter.morel.noguera@gmail.com>
2023-03-16 15:37:08 +0000

    test

---
+++ prog/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h> /* Biblioteca para definir operaciones de entrada y salida */
+
+int main () {
+
+        /* se mostrará por pantalla “Hola mundo” */
+
+        printf(“Hola, mundo”);
+
+return 0;
+retu
+
+}
