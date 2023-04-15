deae6cfce357b9db0d2db1f5798d82107d551dad
walternoguera <walter.morel.noguera@gmail.com>
2023-04-15 19:10:33 +0200

    Depuración de variables, Análisis de requisitos y concepto de RFTP

---
+++ ed/temarios/sesion3/src/Operaciones.java
@@ -0,0 +1,19 @@
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		int a = 3;
+		// int b;
+		// int c = 10;
+
+		if (a > 0) {
+			System.out.println("Es mayor que cero");
+		}
+		if (a < 0) {
+			System.out.println("Es menor que cero");
+		}
+		if (a == 0) {
+			System.out.println("Es igual que cero");
+		}
+	}
+}
