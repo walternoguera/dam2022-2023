1c7942104d1712bd0bebd6de5c1cf28e9a32bd8b
walternoguera <walter.morel.noguera@gmail.com>
2023-04-21 22:40:10 +0200

    Juego: Piedra, Papel, Tijeras - Utilizando bucle 'for', 'do while' y 'Switch'

--- prog/temarios/sesion11/src/sesion11/PiedraPapelTijera.java
+++ prog/temarios/sesion11/src/sesion11/PiedraPapelTijera.java
@@ -1,34 +1,90 @@
 package sesion11;
 
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class PiedraPapelTijera {
 
 	public static void main(String[] args) {
-		char reset;
-		Scanner sc = new Scanner(System.in);
+		// Inicializamos variables
+		Random azar = new Random();
+		Scanner lector = new Scanner(System.in);
+		int maquina;
+		int jugador = 0;
+		int contMaquina = 0;
+		int contJugador = 0;
+		int contEmpates = 0;
+		int rondas;
+
+		// Con una estructura 'do while' le preguntamos al usuario cuantas rondas quiere
+		// jugar
 
 		do {
-			System.out.println("Dame tu nota: ");
-			int nota = sc.nextInt();
-			if (nota < 0 || nota > 10) {
-				System.out.println("Error");
-			} else {
-				if (nota < 5) {
-					System.out.println("Suspenso");
-				} else if (nota >= 5 && nota < 7) {
-					System.out.println("Aprobado");
-				} else if (nota >= 7 && nota < 9) {
-					System.out.println("Notable");
-				} else {
-					System.out.println("Sobresaliente");
+			System.out.print("[sesion11@java ~]$ ¿Cúantas rondas quieres jugar?:  ");
+			rondas = lector.nextInt();
+			lector.nextLine();
+		} while (rondas < 1 || rondas > 10);
+
+		/*------------------------------------------------------------------------------------*/
+		for (int i = 1; i <= rondas; i++) {
+			System.out.println("===========================================");
+			System.out.println("[sesion11@java ~]$ RONDA: " + i);
+
+			// maq: azar p,p,t
+
+			maquina = azar.nextInt(3);// 0.1.2
+
+			// jugador p,p,t
+
+			do {
+				System.out.print("[sesion11@java ~]$ Decide: \n 0-Papel, 1-Piedra, 2-Tijeras: ");
+				jugador = lector.nextInt();
+				lector.nextLine();
+			} while (jugador < 0 || jugador > 2);
+
+			// comparar
+
+			switch (maquina) {
+			case 0:
+				switch (jugador) {
+				case 0:
+					contEmpates++;
+					break;
+				case 1:
+					contMaquina++;
+					break;
+				default:
+					contJugador++;
 				}
+				break;
+			case 1:
+				switch (jugador) {
+				case 0:
+					contJugador++;
+					break;
+				case 1:
+					contEmpates++;
+					break;
+				default:
+					contMaquina++;
+				}
+				break;
+			case 2:
+				switch (jugador) {
+				case 0:
+					contMaquina++;
+					break;
+				case 1:
+					contJugador++;
+					break;
+				default:
+					contEmpates++;
+				}
+				break;
 			}
-			System.out.println("¿Quieres empezar de nuevo? s/n: ");
-			reset = sc.next().charAt(0);
-		} while (reset == 's' || reset == 'S');
-		sc.close();
-		System.out.println("Hasta luego!");
+			System.out.println("Empates:" + contEmpates);
+			System.out.println("Jugador:" + contJugador);
+			System.out.println("Máquina:" + contMaquina);
+		}
 	}
 }
