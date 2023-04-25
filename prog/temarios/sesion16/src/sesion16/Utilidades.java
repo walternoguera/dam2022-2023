49344d86da194c723b2cddcadb38e1269858c594
walternoguera <walter.morel.noguera@gmail.com>
2023-04-25 13:53:52 +0200

    Refactorizando código, cambiando la salida del metodo

--- prog/temarios/sesion16/src/sesion16/Utilidades.java
+++ prog/temarios/sesion16/src/sesion16/Utilidades.java
@@ -5,6 +5,6 @@ public class Utilidades {
 
 	public static void warning(String text) {
 		// devuelve un texto
-		System.out.println("WARNING: Ambos nombres tienen la misma longitud");
+		System.out.println("WARNING: Ambos nombres coinciden con la longitud de caracteres.");
 	}
 }
