a2cc01bddc40f0c0fa72aba19530b25b4daed721
walternoguera <walter.morel.noguera@gmail.com>
2023-04-26 16:48:03 +0200

    Creando un buscador sencillo con indexOf

--- prog/temarios/sesion17/src/sesion17/Validaciones.java
+++ prog/temarios/sesion17/src/sesion17/Validaciones.java
@@ -1,15 +1,44 @@
 package sesion17;
-
+import java.util.Scanner;
 public class Validaciones {
 
 	public static void main(String[] args) {
-		String nombre = "pepepepe";
-		int posicion;
+		String[] cadena = new String [2];
+		String buscar;
+		Scanner lector = new Scanner(System.in);
+		int posicion = -1;
 		
+		
+		for (int i = 0; i <= 1; i++) {
+			System.out.print((i +1) + ". Añade un nombre: ");
+			cadena[i] = lector.nextLine();
+		}
+		
+		
+		// buscador
+		System.out.println("=============================");
+		System.out.println("Que nombre quieres buscar?: ");
+		buscar = lector.nextLine();
+		
+		for (int i = 0; i <= cadena.length; i++) {
+			if (cadena[i].indexOf(buscar) >= 0) {
+				posicion = i;
+				break;
+			}
+		}
+		
+		if (posicion >= 0) {
+            System.out.println("El nombre " + buscar + " está en la posición " + posicion);
+        } else {
+            System.out.println("El nombre " + buscar + " no se encuentra en el array");
+        }
+		
+		/*
 		posicion = nombre.indexOf('e');
 		
 		System.out.println("pos " + nombre.indexOf('e', posicion +1));
 		System.out.println("posicion final " + nombre.lastIndexOf('e'));
+		System.out.println("pos" + nombre.indexOf('e', 4));*/
 	}
 
 }
