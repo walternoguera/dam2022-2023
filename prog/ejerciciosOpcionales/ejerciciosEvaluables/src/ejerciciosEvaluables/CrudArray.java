fd3fd744b7f318652b21b53254e4a8504edffc73
walternoguera <walter.morel.noguera@gmail.com>
2023-05-03 23:27:37 +0200

    añadiendo directorio

---
+++ prog/ejerciciosOpcionales/ejerciciosEvaluables/src/ejerciciosEvaluables/CrudArray.java
@@ -0,0 +1,27 @@
+package ejerciciosEvaluables;
+
+public class CrudArray {
+
+	public static void main(String[] args) {
+		/*
+		 * Se trata de realizar un CRUD con Arrays.
+		 * Elige un tema que te interese y guarda al menos 3 datos relacionados en 3
+		 * arrays en paralelo. Por ejemplo, cine, en un array guardamos los títulos, en
+		 * otro los géneros y en otro el año de creación...
+		 * Se deben guardar un máximo de 20 películas.
+		 * Si el array está lleno se debe informar al usuario.
+		 * Igualmente si no tiene contenido no se permitirá modificar o eliminar en esa
+		 * posición. Debe aparecer un menú que pida al usuario la acción a realizar (añadir,
+		 * modificar, borrar el contenido de la posición del array, buscar por
+		 * cualquiera de los datos guardados y finalmente salir). El programa repetirá
+		 * el menú hasta que se decida terminar.
+		 */
+		
+		String[] titulos = new String[20];
+		String[] generos = new String[20];
+		int[] año = new int[20];
+		
+
+	}
+
+}
