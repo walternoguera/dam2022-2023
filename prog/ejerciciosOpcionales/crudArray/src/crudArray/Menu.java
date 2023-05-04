19deb590a8e80c4b543c3c8799d962e408b3222f
walternoguera <walter.morel.noguera@gmail.com>
2023-05-04 13:49:28 +0200

    Refactorizando código

--- prog/ejerciciosOpcionales/crudArray/src/crudArray/Menu.java
+++ prog/ejerciciosOpcionales/crudArray/src/crudArray/Menu.java
@@ -3,7 +3,7 @@ package crudArray;
 public class Menu {
 	public static void añadir(String[] titulos, String[] generos, int[] anio) {
 		for (int i = 0; i< 3;i++) {
-			titulos[i] = Utilidades.pedirString("▓▓▓▓▓Titulo: ");
+			titulos[i] = Utilidades.pedirString("Titulo: ");
 			generos[i] = Utilidades.pedirString("Genero: ");
 			anio[i] = Utilidades.pedirDatos("Año: ");
 		}
