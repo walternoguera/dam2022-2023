17badbfd4d94438b27fc8c68254e362d30e254ea
walternoguera <walter.morel.noguera@gmail.com>
2023-05-14 21:11:00 +0200

    ArrayList, Try, métodos

---
+++ prog/temarios/sesion26/src/sesion26/Controlador.java
@@ -0,0 +1,67 @@
+package sesion26;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Controlador {
+
+	public static void main(String[] args) {
+		int[] arrayInt = new int[3];
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		ArrayList<String> palabras = new ArrayList<String>();
+		ArrayList<Float> numeros = new ArrayList<Float>();
+
+		try {
+			// CRUD
+			// create
+			arrayInt[0] = 12;
+			lista.add(12);
+			lista.add(2);
+			lista.add(44);
+			lista.add(23);
+			lista.add(12);
+			lista.add(1);
+			lista.add(26);
+			lista.add(30);
+			lista.add(5);
+			lista.add(7);
+			palabras.add("paula");
+		
+
+			// mostrar contenido
+			System.out.println(Arrays.toString(arrayInt));
+			System.out.println(lista.toString());
+		
+
+			// modificar
+			arrayInt[0] = 99;
+			lista.set(1, 44);
+			System.out.println(Arrays.toString(arrayInt) + lista.toString());
+
+			// borrar
+			// arrayInt[0] = 99; en array no se puede borrar
+			lista.remove(1);
+			System.out.println(Arrays.toString(arrayInt) + lista.toString());
+			
+			//metodos utiles
+			
+			//con collections.sort ordenamos de menor a mayir
+			Collections.sort(lista);
+			System.out.println(lista.toString());
+			
+			//con collections.reverse ordenamos de mayor a menor
+			Collections.reverse(lista);
+			System.out.println(lista.toString());
+			
+			//con collections.shuffle ordenamos de forma random
+			Collections.shuffle(lista);
+			System.out.println(lista.toString());
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("ERROR");
+		}
+		
+		
+		
+	}
+}
---
+++ prog/temarios/sesion26/src/sesion26/Validaciones.java
@@ -0,0 +1,10 @@
+package sesion26;
+
+public class Validaciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
