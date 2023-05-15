5cb10ea11cd1d5cb07397f681ed66599f6a14c58
U-DESKTOP-Q93AT7D\walter <walter.morel.noguera@gmail.com>
2023-05-15 21:39:15 +0200

    Agregando multiples cartas con array

--- ed/temarios/cartas/src/cartas/Controlador.java
+++ ed/temarios/cartas/src/cartas/Controlador.java
@@ -8,16 +8,30 @@ public class Controlador {
 
 		Baraja poker = new Baraja("Francesa", new ArrayList<Carta>());
 		
-		poker.addCarta(new Carta("Corazones", "As", 1));
+		inicializar(poker);
+		
+		/*poker.addCarta(new Carta("Corazones", "As", 1));
 		poker.addCarta(new Carta("Corazones", "Dos", 2));
 		poker.addCarta(new Carta("Trébol", "As", 1));
 		poker.addCarta(new Carta("Trébol", "Dos", 2));
 		poker.addCarta(new Carta("Picas", "As", 1));
-		poker.addCarta(new Carta("Picas", "Dos", 2));
+		poker.addCarta(new Carta("Picas", "Dos", 2));*/
 		
 		System.out.println(poker.toString());
 		poker.barajar();
 		System.out.println(poker.toString());
 		System.out.println(poker.sacarCarta().toString());
+	}
+	
+	private static void inicializar(Baraja mazo) {
+		String[] palos = {"corazones", "picas", "diamantes", "tréboles"};
+		String[] nombres = {"as", "dos", "J", "Q", "K"};
+		int[] valores = {11, 2, 10, 10, 10};
+		
+		for (String palo: palos) {
+			for (int i = 0; i < nombres.length; i++) {
+				mazo.addCarta(new Carta(palo, nombres[i], valores[i]));
+			}
+		}
 	}
 }
