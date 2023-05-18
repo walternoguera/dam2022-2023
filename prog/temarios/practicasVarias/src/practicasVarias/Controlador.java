e7e228dac9197bde2045649df6b397e1d7f8a38b
U-DESKTOP-Q93AT7D\walter <walter.morel.noguera@gmail.com>
2023-05-18 02:03:49 +0200

    refactorizando código

--- prog/temarios/practicasVarias/src/practicasVarias/Controlador.java
+++ prog/temarios/practicasVarias/src/practicasVarias/Controlador.java
@@ -5,10 +5,17 @@ import java.util.Arrays;
 public class Controlador {
 
 	public static void main(String[] args) {
-		String[] nombre = Uti.arrayString();
-		int[] numeros = Uti.arrayInt();
-	
-		System.out.println(Arrays.toString(nombre));
-		System.out.println(Arrays.toString(numeros));
+		/*String[] nombre = Uti.arrayString();
+		int[] numeros = Uti.arrayInt();*/
+		
+		Personas alumno = new Personas("paula", 22, 2001);
+		Personas alumno1 = new Personas("lola", 23, 2000);
+		System.out.println(alumno);
+		alumno.setNombres("pausfl");
+
+		System.out.println(alumno);
+		System.out.println(alumno1);
+		/*System.out.println(Arrays.toString(nombre));
+		System.out.println(Arrays.toString(numeros));*/
 	}
 }
