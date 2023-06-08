a52b2804dc4fff54b2022df915f042a9c361bbd8
walternoguera <walter.morel.noguera@gmail.com>
2023-06-09 00:41:06 +0200

    Autocommit

--- prog/temarios/PersonasFechas/src/caso1/UtilidadesFecha.java
+++ prog/temarios/PersonasFechas/src/caso1/UtilidadesFecha.java
@@ -1,5 +1,13 @@
 package caso1;
 
-public class UtilidadesFecha {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+public class UtilidadesFecha {
+    public static int verEdad(LocalDate fecha){
+        LocalDate ahora = LocalDate.now();
+
+        return (int) ChronoUnit.YEARS.between(fecha, ahora);
+
+    }
 }
